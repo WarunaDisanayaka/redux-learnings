45a952d2678243a00719ffe44bb41b11b82bc34d
Waruna <86902580+WarunaDisanayaka@users.noreply.github.com>
2024-11-29 15:45:38 +0530

    Added input form

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 import store from './store/store'
 import './App.css'
 import { Container, Typography } from '@mui/material'
+import AddHabitForm from './components/add-habit-form'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
           <Typography component='h1' variant='h2' align='center'>
             Habbit Tracker
           </Typography>
+          <AddHabitForm />
         </Container>
       </Provider>
     </div>
